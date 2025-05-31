9e5e5c8345ae9159c43ff19c3e5366218c3ec860
ideal-tomy <ryojitomii@gmail.com>
2025-06-01 05:07:28 +0900

    仮画像02

--- src/pages/ServiceDetail/index.tsx
+++ src/pages/ServiceDetail/index.tsx
@@ -170,7 +170,7 @@ const ServiceDetailPage = () => {
     <Box as="main" bg="gray.50">
       {/* Hero Section with Background Image and Overlay */}
       <Box
-        bgImage="url('/images/test.png')"
+        bgImage="url('/images/service_hero.png')"
         bgPosition="center"
         bgRepeat="no-repeat"
         bgSize="cover"
